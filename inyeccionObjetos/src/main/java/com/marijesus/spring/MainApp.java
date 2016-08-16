f0260e0ab679b93bbfb38073e170d780a74ee685
jose1824 <jose1824@live.com.mx>
2016-08-15 22:35:59 -0500

    Inyección de Objetos

---
+++ inyeccionObjetos/src/main/java/com/marijesus/spring/MainApp.java
@@ -0,0 +1,26 @@
+package com.marijesus.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.marijesus.beans.Persona;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/marijesus/xml/beans.xml");
+		Persona per = (Persona) appContext.getBean("persona");
+		
+		System.out.println(per.getId() + " " 
+				+ per.getNombre() + " " 
+				+ per.getApodo() + " " 
+				+ per.getPais().getNombre() + " "
+				+ per.getPais().getCiudad().getNombre());
+		
+		((ConfigurableApplicationContext) appContext).close();
+
+	}
+
+}
